e56d3e15051bbc2e557c8509bd275f2c80ca0d97
Mukhammad Karimov <brown@mysuperdispatch.com>
2019-09-03 18:18:01 +0500

    feature: Label integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,13 @@ from rentomatic.settings import TestConfig
 @pytest.yield_fixture(scope="function")
 def app():
     return create_app(TestConfig)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--integration", action="store_true", help="run integration tests")
+
+
+def pytest_runtest_setup(item):
+    if "integration" in item.keywords and not item.config.getvalue("integration"):
+        pytest.skip("need --integration option to run")
+
