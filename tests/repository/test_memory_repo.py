6e7275f966b664d15c99aebffd3f238807a1a013
Mukhammad Karimov <brown@mysuperdispatch.com>
2019-09-03 17:59:12 +0500

    feature: Rooms endpoint with filters

--- tests/repository/test_memory_repo.py
+++ tests/repository/test_memory_repo.py
@@ -43,3 +43,59 @@ def test_repository_list_without_parameters(room_dicts):
     rooms = [r.Room.from_dict(i) for i in room_dicts]
 
     assert repo.list() == rooms
+
+
+def test_repository_list_with_code_equal_filter(room_dicts):
+    repo = memory_repository.MemoryRepo(room_dicts)
+    rooms = [r.Room.from_dict(i) for i in room_dicts]
+
+    repo_rooms = repo.list(filters={"code__eq": "eed76e77-55c1-41ce-985d-ca49bf6c0585"})
+
+    assert len(repo_rooms) == 1
+    assert repo_rooms[0].code == "eed76e77-55c1-41ce-985d-ca49bf6c0585"
+
+
+def test_repository_list_with_price_equal_filter(room_dicts):
+    repo = memory_repository.MemoryRepo(room_dicts)
+    rooms = [r.Room.from_dict(i) for i in room_dicts]
+
+    repo_rooms = repo.list(filters={"price__eq": 60})
+
+    assert len(repo_rooms) == 1
+    assert repo_rooms[0].code == "913694c6-435a-4366-ba0d-da5334a611b2"
+
+
+def test_repository_list_with_price_less_then_filter(room_dicts):
+    repo = memory_repository.MemoryRepo(room_dicts)
+    rooms = [r.Room.from_dict(i) for i in room_dicts]
+
+    repo_rooms = repo.list(filters={"price__lt": 60})
+
+    assert len(repo_rooms) == 2
+    assert set([r.code for r in repo_rooms]) == {
+        "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+        "eed76e77-55c1-41ce-985d-ca49bf6c0585",
+    }
+
+
+def test_repository_list_with_price_greater_then_filter(room_dicts):
+    repo = memory_repository.MemoryRepo(room_dicts)
+    rooms = [r.Room.from_dict(i) for i in room_dicts]
+
+    repo_rooms = repo.list(filters={"price__gt": 60})
+
+    assert len(repo_rooms) == 1
+    assert set([r.code for r in repo_rooms]) == {"fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a"}
+
+
+def test_repository_list_with_price_between_filter(room_dicts):
+    repo = memory_repository.MemoryRepo(room_dicts)
+    rooms = [r.Room.from_dict(i) for i in room_dicts]
+
+    repo_rooms = repo.list(filters={"price__gt": 50, "price__lt": 70})
+
+    assert len(repo_rooms) == 2
+    assert set([r.code for r in repo_rooms]) == {
+        "913694c6-435a-4366-ba0d-da5334a611b2",
+        "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
+    }
